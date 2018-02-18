adda5ec59f29adbb19d154d3fc002dacc974c867
aniket umbc <aniket.umbc@gmail.com>
2018-02-18 17:19:21 +0530

    Add controller Lec-10

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var myApp= angular.module("myApp",[]);
 myApp.config(function(){});
 myApp.run(function(){});
 
-myApp.controller('MyappController',function($scope){
+myApp.controller('MyappController',[$scope, function($scope){
 
     $scope.message="Hello All how's you";
     $scope.names=['aniket','yahoo','Mind','Yom','Tom'];
 
-});
+}]);
