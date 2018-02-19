5391ef76c82a199705cd21fef2e9f9644494ac90
aniket umbc <aniket.umbc@gmail.com>
2018-02-19 15:16:58 +0530

    Filter Add

--- app.js
+++ app.js
@@ -3,9 +3,30 @@ var myApp= angular.module("myApp",[]);
 myApp.config(function(){});
 myApp.run(function(){});
 
-myApp.controller('MyappController',[$scope, function($scope){
+myApp.controller('MyappController', ['$scope', function($scope){
 
     $scope.message="Hello All how's you";
-    $scope.names=['aniket','yahoo','Mind','Yom','Tom'];
+    $scope.names=[
+        {
+            name:'Aniket',
+            color:'green'
+        },
+        {
+            name:'Bunny',
+            color:'red'
+        },
+        {
+            name:'Don',
+            color:'blue'
+        },
+        {
+            name:'Mike',
+            color:'orange'
+        },
+        {
+            name:'zeong',
+            color:'yellow'
+        },
+    ];
 
 }]);
